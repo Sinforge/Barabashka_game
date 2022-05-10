b0fd950db2eed092936715b20efc47e04a2c3d78
Sinforge <vlad.vlasov77@mail.ru>
2022-05-10 21:39:36 +0300

    add activity for choosing game mode

--- app/src/main/java/ru/sinforge/barabashka_game/Activities/Activity_for_play.java
+++ app/src/main/java/ru/sinforge/barabashka_game/Activities/Activity_for_play.java
@@ -1,26 +1,20 @@
 package ru.sinforge.barabashka_game.Activities;
 
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import ru.sinforge.barabashka_game.GameComponents.GameView;
 import ru.sinforge.barabashka_game.GameComponents.PauseFragment;
+import ru.sinforge.barabashka_game.Services.MusicService;
 import ru.sinforge.barabashka_game.R;
 
 public class Activity_for_play extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        startService(new Intent(this, MusicService.class));
         setContentView(R.layout.activity_for_play);
         // Do something in response to button click
         Button pause = findViewById(R.id.btn);
